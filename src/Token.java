8aa31617dc3ab9a14ddf8a2e51cd79f191caa925
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-03-02 16:14:02 -0700

    Implemented Token

--- src/Token.java
+++ src/Token.java
@@ -1,3 +1,18 @@
 public class Token {
-    // TODO: Not implemented stub class.
+    String kind, data;
+
+    public Token (String id) {
+        kind = "string";
+        data = id;
+    }
+
+    public Token (double value) {
+        kind = "number";
+        data = Double.toString(value);
+    }
+
+    @Override
+    public String toString() {
+        return "Kind: " + kind + "token: " + data;
+    }
 }
