b9f5380b1f226eda3eb1d103df0c6080a35a3b65
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-03-02 16:24:32 -0700

    Death to plain for

--- src/CalcLang.java
+++ src/CalcLang.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 public class CalcLang {
     public static void main (String[] args) {
         Lexer lex = new Lexer(args[0]);
-        ArrayList<Token> list = lex.scan();
-        for (int k = 0; k < list.size(); k++) {
-            System.out.println(list.get(k));
+        ArrayList<Token> tokens = lex.scan();
+        for (Token token : tokens) {
+            System.out.println(token);
         }
     }
 }
