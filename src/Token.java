aa301ec3afa2b2feb6d87f6cac765b77a9e9face
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-03-02 22:45:50 -0700

    Give tokens awareness of where they're from

--- src/Token.java
+++ src/Token.java
@@ -1,18 +1,23 @@
 public class Token {
-    String kind, data;
+    String kind, data, line;
+    int lineNum;
 
-    public Token (String id) {
+    public Token (String id, String line, int lineNum) {
         kind = "string";
         data = id;
+        this.line = line;
+        this.lineNum = lineNum;
     }
 
-    public Token (double value) {
+    public Token (double value, String line, int lineNum) {
         kind = "number";
         data = Double.toString(value);
+        this.line = line;
+        this.lineNum = lineNum;
     }
 
     @Override
-    public String toString() {
+    public String toString () {
         return "Kind: " + kind + "token: " + data;
     }
 }
