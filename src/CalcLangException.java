4b1b9272fff5084b74ef3733984e8d1b924ae35d
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-03-02 18:16:09 -0700

    Added My Exceptions
    For better clarity in my meaning

---
+++ src/CalcLangException.java
@@ -0,0 +1,9 @@
+public class CalcLangException extends java.lang.Exception {
+    public CalcLangException () {
+        super();
+    }
+
+    public CalcLangException (String message) {
+        super(message);
+    }
+}
---
+++ src/LexingException.java
@@ -0,0 +1,9 @@
+public class LexingException extends CalcLangException {
+    public LexingException () {
+        super();
+    }
+
+    public LexingException (String message) {
+        super(message);
+    }
+}
