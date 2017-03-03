79f94b63cbe16e5dbe77ba34c39863e1418b3aaa
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-03-02 18:17:34 -0700

    Using new exception

--- src/CalcLang.java
+++ src/CalcLang.java
@@ -3,7 +3,15 @@ import java.util.ArrayList;
 public class CalcLang {
     public static void main (String[] args) {
         Lexer lex = new Lexer(args[0]);
-        ArrayList<Token> tokens = lex.scan();
+        ArrayList<Token> tokens;
+        try {
+            tokens = lex.scan();
+        } catch (LexingException ex){
+            System.err.println(ex.getMessage());
+            System.exit(-2);
+            return;
+        }
+
         for (Token token : tokens) {
             System.out.println(token);
         }
--- src/Lexer.java
+++ src/Lexer.java
@@ -19,10 +19,11 @@ public class Lexer {
         }
     }
 
-    public ArrayList<Token> scan () {
-        ArrayList<Token> tokens = new ArrayList<Token>();
+    public ArrayList<Token> scan () throws LexingException {
+        ArrayList<Token> tokens = new ArrayList<>();
 
         int state = 0;
+        int lineNum = 0;
 
         String id = "";
         boolean isId = false;
@@ -31,26 +32,40 @@ public class Lexer {
 
         while (input.hasNext()) {
             String s = input.nextLine();
+            lineNum++;
             for (int k = 0; k < s.length(); k++) {
                 // process next individual char in the file
                 char x = s.charAt(k);
 
-                if (state == 0) {
+                if (state == 0) { // Start token: Strip beginning whitespace before token.
                     if (isWhiteSpace(x)) {
-                        // stay in state 0
+                        // Stripping whitespace
                     } else if (isLetter(x)) {
                         state = 1;
                         id += x;
                         isId = true;
                     } else if (isDigit(x)) {
                         state = 3;
-                        value = 10 * value + x - '0';
+                        value = 10 * value + (x - '0');
                         isValue = true;
+                    } else if (isMathSymbol(x)) {
+                        state = 2;
+                        // Do anything?
+                    } else if (isMinusSymbol(x)) {
+                        state = 5;
+                    } else {
+                        throw new LexingException("Unexpected symbol: " + x + " encountered on line" + lineNum + ".");
                     }
-                } else if (state == 1) {
-                } else if (state == 2) {
-                } else if (state == 3) {
-                } else if (state == 4) {
+                } else if (state == 1) { // Identifier token: add letters and digits to running name `id`
+                    if (isLetter(x) || isDigit(x)) {
+                        id += x;
+                    } else {
+                        throw new UnsupportedOperationException(); // TODO: handle end of token
+                    }
+                } else if (state == 2) { // Math token: math symbol alone
+                } else if (state == 3) { // Number token (integer part): Consume digits to running total `value`
+                } else if (state == 4) { // Number token (decimal part): Consume digits to running total `value`
+                } else if (state == 5) { // Minus state: Decide negative number or math symbol.
                 }
             }
         }
@@ -76,6 +91,14 @@ public class Lexer {
 
     private boolean isDigit (char x) {
         return x >= '0' && x <= '9';
+    }
+
+    private boolean isMathSymbol (char x) {
+        return "=+/*()".indexOf(x) != -1;
+    }
+
+    private boolean isMinusSymbol (char x) {
+        return x == '-';
     }
 
 }
