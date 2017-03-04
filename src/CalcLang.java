2ef044afa4a2912a325bd13374c5af1a903adb17
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-03-04 00:23:19 -0700

    Renamed vars in Lexer
    Also changed who deals with FileNotFound and continuing work on the FSA

--- src/CalcLang.java
+++ src/CalcLang.java
@@ -1,12 +1,22 @@
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class CalcLang {
     public static void main (String[] args) {
-        Lexer lex = new Lexer(args[0]);
+        Lexer lex;
         ArrayList<Token> tokens;
+
         try {
-            tokens = lex.scan();
-        } catch (LexingException ex){
+            lex = new Lexer(args[0]);
+        } catch (FileNotFoundException ex) {
+            System.err.print("Source file '" + args[0] + "' not found.");
+            System.exit(-1);
+            return;
+        }
+
+        try {
+            tokens = lex.tokenize();
+        } catch (LexingException ex) {
             System.err.println(ex.getMessage());
             System.exit(-2);
             return;
--- src/Lexer.java
+++ src/Lexer.java
@@ -4,6 +4,8 @@
 */
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,71 +13,80 @@ public class Lexer {
 
     private Scanner input;
 
-    public Lexer (String fileName) {
-        try {
-            input = new Scanner(new File(fileName));
-        } catch (Exception e) {
-            System.out.println("Source file: " + fileName + " not found.");
-        }
+    public Lexer (String fileName) throws FileNotFoundException {
+        input = new Scanner(new File(fileName));
     }
 
-    public ArrayList<Token> scan () throws LexingException {
+    public ArrayList<Token> tokenize () throws LexingException {
         ArrayList<Token> tokens = new ArrayList<>();
 
-        int state = 0;
+        int fsaState = 0;
         int lineNum = 0;
 
-        String id = "";
-        boolean isId = false;
-        double value = 0;
-        boolean isValue = false;
+        String stringValue = "";
+        boolean isStringToken = false;
+        BigDecimal numberValue = BigDecimal.ZERO;
+        int decimalPlaces = 0;
+        boolean isNumberToken = false;
 
         while (input.hasNext()) {
-            String s = input.nextLine();
+            String sourceLine = input.nextLine();
             lineNum++;
-            for (int k = 0; k < s.length(); k++) {
+            for (int charNum = 0; charNum < sourceLine.length(); charNum++) {
                 // process next individual char in the file
-                char x = s.charAt(k);
+                char currentChar = sourceLine.charAt(charNum);
 
-                if (state == 0) { // Start token: Strip beginning whitespace before token.
-                    if (isWhiteSpace(x)) {
+                if (fsaState == 0) { // Start token: Strip beginning whitespace before token.
+                    if (isWhiteSpace(currentChar)) {
                         // Stripping whitespace
-                    } else if (isLetter(x)) {
-                        state = 1;
-                        id += x;
-                        isId = true;
-                    } else if (isDigit(x)) {
-                        state = 3;
-                        value = 10 * value + (x - '0');
-                        isValue = true;
-                    } else if (isMathSymbol(x)) {
-                        state = 2;
-                        // Do anything?
-                    } else if (isMinusSymbol(x)) {
-                        state = 5;
+                    } else if (isLetter(currentChar)) {
+                        fsaState = 1;
+                        stringValue += currentChar;
+                        isStringToken = true;
+                    } else if (isDigit(currentChar)) {
+                        fsaState = 3;
+                        numberValue = numberValue.multiply(BigDecimal.TEN).add(BigDecimal.valueOf(currentChar - '0'));
+                        isNumberToken = true;
+                    } else if (isMathSymbol(currentChar)) {
+                        fsaState = 2;
+                        // TODO: handle end of token
+                        throw new UnsupportedOperationException("Not Implemented Yet.");
                     } else {
-                        throw new LexingException("Unexpected symbol: " + x + " encountered on line" + lineNum + ".");
+                        throw LexingException.newUnexpectedSymbol(currentChar, charNum, lineNum, sourceLine);
                     }
-                } else if (state == 1) { // Identifier token: add letters and digits to running name `id`
-                    if (isLetter(x) || isDigit(x)) {
-                        id += x;
+                } else if (fsaState == 1) { // Identifier token: add letters and digits to running name `stringValue`
+                    if (isLetter(currentChar) || isDigit(currentChar)) {
+                        stringValue += currentChar;
                     } else {
-                        throw new UnsupportedOperationException(); // TODO: handle end of token
+                        // TODO: handle end of token
+                        throw new UnsupportedOperationException("Not Implemented Yet.");
                     }
-                } else if (state == 2) { // Math token: math symbol alone
-                } else if (state == 3) { // Number token (integer part): Consume digits to running total `value`
-                } else if (state == 4) { // Number token (decimal part): Consume digits to running total `value`
-                } else if (state == 5) { // Minus state: Decide negative number or math symbol.
+                } else if (fsaState == 3) { // Number token (integer part): Consume digits to running total `numberValue`
+                    if (currentChar == '.') {
+                        fsaState = 4;
+                        decimalPlaces = 1;
+                    } else if (isDigit(currentChar)) {
+                        numberValue = numberValue.multiply(BigDecimal.TEN).add(BigDecimal.valueOf(currentChar - '0'));
+                    } else {
+                        // TODO: handle ending token
+                        throw new UnsupportedOperationException("Not Implemented Yet.");
+                    }
+                } else if (fsaState == 4) { // Number token (decimal part): Consume digits to running total `numberValue`
+                    if (isDigit(currentChar)) {
+                        numberValue = numberValue.add(BigDecimal.valueOf((currentChar - '0') / (10 * decimalPlaces)));
+                        decimalPlaces++;
+                    } else {
+                        // TODO: handle ending token
+                        throw new UnsupportedOperationException("Not Implemented Yet.");
+                    }
                 }
             }
-        }
 
-        if (isId) {
-            tokens.add(new Token(id));
-        } else if (isValue) {
-            tokens.add(new Token(value));
-        } else {
-            // Stay empty
+            if (isStringToken) {
+                tokens.add(new Token(stringValue, sourceLine, lineNum));
+            } else if (isNumberToken) {
+                tokens.add(new Token(numberValue, sourceLine, lineNum));
+            }
         }
 
         return tokens;
@@ -94,11 +105,7 @@ public class Lexer {
     }
 
     private boolean isMathSymbol (char x) {
-        return "=+/*()".indexOf(x) != -1;
-    }
-
-    private boolean isMinusSymbol (char x) {
-        return x == '-';
+        return "=+-/*()".indexOf(x) != -1;
     }
 
 }
