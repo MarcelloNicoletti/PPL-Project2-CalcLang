f946c6b2bd559dbc4c1d575ada08f7314eff1b86
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-04-12 17:07:47 -0600

    Implement Executor functionality.
    Also changed number type to be double rather than BigDecimal

--- src/Executor.java
+++ src/Executor.java
@@ -1,48 +1,106 @@
-import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Executor {
-    private Map<String, BigDecimal> memory;
+    private Map<String, Double> memory;
+    private Scanner input;
 
-    public Executor (Set<String> identifierSet) {
+    public Executor (Set<String> identifierSet, Scanner input) {
         this.memory = new HashMap<>();
         for (String identifier : identifierSet) {
-            memory.put(identifier, BigDecimal.ZERO);
+            memory.put(identifier, 0.0);
         }
+        this.input = input;
     }
 
     public void execute (Node node) {
+        if (node == null) {
+            return;
+        }
         switch (node.getNodeType()) {
             case PROGRAM:
+                execute(node.getChild(0));
                 break;
             case STATEMENTS:
-                break;
-            case STATEMENT:
+                execute(node.getChild(0));
+                execute(node.getChild(1));
                 break;
             case MESSAGE:
+                System.out.print(node.getData());
                 break;
             case INPUT:
+                System.out.print(node.getData());
+                double value = input.nextDouble();
+                memory.put(node.getChild(0).getData(), value);
                 break;
             case ASSIGNMENT:
+                memory.put(node.getChild(0).getData(), evaluate(node.getChild(1)));
+                break;
+            case NEWLINE:
+                System.out.println();
+                break;
+            case SHOW:
+                System.out.print(evaluate(node.getChild(0)));
                 break;
         }
     }
 
-    public BigDecimal evaluate (Node node) {
+    public double evaluate (Node node) {
+        double left, right;
         switch (node.getNodeType()) {
             case VARIABLE:
                 return memory.get(node.getData());
-            case EXPRESSION:
-                break;
-            case TERM:
-                break;
-            case FACTOR:
-                break;
             case NUMBER:
-                return new BigDecimal(node.getData());
+                return Double.parseDouble(node.getData());
+            case EXPRESSION:
+                left = evaluate(node.getChild(0));
+                if (node.getData().isEmpty()) {
+                    return left;
+                } else if (node.getData().equals("+")) {
+                    right = evaluate(node.getChild(1));
+                    return left + right;
+                } else if (node.getData().equals("-")) {
+                    right = evaluate(node.getChild(1));
+                    return left - right;
+                } else {
+                    // TODO: throw Executor error
+                    return 0.0;
+                }
+            case TERM:
+                left = evaluate(node.getChild(0));
+                if (node.getData().isEmpty()) {
+                    return left;
+                } else if (node.getData().equals("*")) {
+                    right = evaluate(node.getChild(1));
+                    return left * right;
+                } else if (node.getData().equals("/")) {
+                    right = evaluate(node.getChild(1));
+                    return left / right;
+                } else {
+                    // TODO: throw Executor error
+                    return 0.0;
+                }
+            case FACTOR:
+                double value = evaluate(node.getChild(0));
+                if (node.getData().equals("-")) {
+                    value *= -1;
+                }
+                return value;
+            case BIFN:
+                double argument = evaluate(node.getChild(0));
+                switch (node.getData()) {
+                    case "sqrt":
+                        return Math.sqrt(argument);
+                    case "sin":
+                        return Math.sin(argument);
+                    case "cos":
+                        return Math.cos(argument);
+                }
+            default:
+                // TODO: throw Executor error
+                return 0.0;
         }
-        return null;
     }
 }
