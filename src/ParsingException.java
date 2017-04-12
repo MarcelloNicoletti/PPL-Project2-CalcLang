10fdd8b4b26bbd337bfbf4ab72a77416644ffbf0
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-04-12 01:55:39 -0600

    Added ParsingException factory

--- src/ParsingException.java
+++ src/ParsingException.java
@@ -7,6 +7,19 @@ public class ParsingException extends CalcLangException {
         super(message);
     }
 
+    public static ParsingException newFromToken (String message, Token token) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(message).append("\n");
+        sb.append("from token '").append(token.getValue()).append("' on line ").append(token.getLineNumber()).append(" at carrot:\n");
+        sb.append(token.getLineNumber()).append(": ").append(token.getSourceLine()).append("\n");
+        final int leadingSpaceSize = token.getStartChar() + Integer.toString(token.getLineNumber()).length() + 2;
+        for (int i = 0; i < leadingSpaceSize; i++) {
+            sb.append(" ");
+        }
+        sb.append("^");
+        return new ParsingException(sb.toString());
+    }
+
     // TODO: Add factory methods here.
 
 //    public static ParsingException factoryBlank () {
