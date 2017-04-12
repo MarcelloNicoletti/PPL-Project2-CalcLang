6776b471aefd8fb545c2aadaf605de78547483fd
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-04-12 01:09:16 -0600

    added toString for lexer tokentypes

--- src/TokenType.java
+++ src/TokenType.java
@@ -7,4 +7,26 @@ public enum TokenType {
     BIFN,
     EOF,
     ;
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case KEY:
+                return "keyword";
+            case ID:
+                return "identifier";
+            case NUM:
+                return "number";
+            case MATH:
+                return "math symbol";
+            case STR:
+                return "string literal";
+            case BIFN:
+                return "built in function";
+            case EOF:
+                return "end of file";
+            default:
+                return "how did this happen?";
+        }
+    }
 }
